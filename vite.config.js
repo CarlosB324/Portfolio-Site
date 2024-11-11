55a8b6221b9df4c5b3998a7276a87bb9a552953c
CarlosB324 <127149062+CarlosB324@users.noreply.github.com>
2024-11-11 13:19:00 -0500

    added build.rollupOptions.external to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
+import "firebase/app"
+import "firebase/firestore/lite"
 
 export default defineConfig({
-	plugins: [react()]	
+	plugins: [
+		react()
+	],
+	build: {
+		rollupOptions: {
+		  external: ['firebase/app , firebase/firestore/lite']
+		}
+	}
 })
